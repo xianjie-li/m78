29a5d914112477f92d2e5d9e507e71b5f2cf7bf1
lxj <1073830130@qq.com>
2020-04-29 00:31:54 +0800

    popper组件原形

--- src/components/popper/popper.tsx
+++ src/components/popper/popper.tsx
@@ -5,33 +5,53 @@ import { useSpring, animated, interpolate, config } from 'react-spring';
 import cls from 'classnames';
 import { useUpdateEffect } from 'react-use';
 import _throttle from 'lodash/throttle';
-import { GetBoundMetasDirectionKeys, getPopperMetas } from './getPopperMetas';
+import {isDom, isNumber} from '@lxjx/utils';
+import {GetBoundMetasDirectionKeys, getPopperMetas} from './getPopperMetas';
 
 interface PopperProps {
   /** 直接指定目标元素 */
   targetEl?: HTMLElement;
   /** 气泡方向 */
   direction?: GetBoundMetasDirectionKeys;
+  /** 可选的子元素 */
   children?: React.ReactElement;
+  /** 包裹元素，作为气泡边界的标识，并会在滚动时对气泡进行更新, 默认情况下，边界为窗口，并在window触发滚动时更新气泡 */
+  wrapEl?: HTMLElement | React.MutableRefObject<any>;
 }
 
-const Popper: React.FC<PopperProps> = ({ children, direction = 'top' }) => {
+/** 传入dom时原样返回，传入执行dom对象的ref时返回current，否则返回undefined */
+function getRefDomOrDom(
+  target?: HTMLElement | React.MutableRefObject<any>,
+): HTMLElement | undefined {
+  if (!target) return undefined;
+  if (isDom(target)) return target;
+  if (target && isDom(target.current)) return target.current as HTMLElement;
+  return undefined;
+}
+
+const Popper: React.FC<PopperProps> = ({children, direction = 'top', wrapEl}) => {
   const popperEl = useRef<HTMLDivElement>(null!);
   const targetEl = useRef<any>(null!);
-  const wrapEl = useRef<any>(null!);
 
   const self = useSelf({
-    // 用于跳过初次动画
+    // 优化动画
     refreshCount: 0,
+    /** 气泡最近一次获取的x轴位置，用于减少更新 */
     lastX: (undefined as unknown) as number,
+    /** 气泡最近一次获取的y轴位置，用于减少更新 */
     lastY: (undefined as unknown) as number,
+    /** 最近一次的可见状态，用于：优化显示效果、提高性能 */
     lastVisible: true,
+    /** 最后获取到的气泡宽度 */
     lastPopperW: 0,
+    /** 最后获取到的气泡高度 */
     lastPopperH: 0,
   });
 
   const [state, setState] = useSetState({
+    /** 气泡所在方向 */
     direction: direction as GetBoundMetasDirectionKeys,
+    /** 是否可见 */
     show: true,
   });
 
@@ -44,6 +64,7 @@ const Popper: React.FC<PopperProps> = ({ children, direction = 'top' }) => {
     config: config.stiff,
   }));
 
+  /** 保存气泡尺寸，由于有缩放动画，直接获取dom信息会出现偏差 */
   useEffect(() => {
     if (state.show) {
       self.lastPopperW = popperEl.current.offsetWidth;
@@ -51,26 +72,25 @@ const Popper: React.FC<PopperProps> = ({ children, direction = 'top' }) => {
     }
   });
 
-  console.log(123);
-
+  /** 更新气泡位置、状态、显示等 */
   const refresh = useFn(
     () => {
-      if (!popperEl.current || !targetEl.current) return;
+      if (!targetEl.current) return;
+      if (!isNumber(self.lastPopperW) || !isNumber(self.lastPopperH)) return;
 
-      const { currentDirection, currentDirectionKey, visible } = getPopperMetas(
-        // popperEl.current,
-        { width: self.lastPopperW, height: self.lastPopperH },
+      const {currentDirection, currentDirectionKey, visible} = getPopperMetas(
+        {width: self.lastPopperW, height: self.lastPopperH},
         targetEl.current,
         {
           offset: 12,
-          wrap: wrapEl.current,
+          wrap: getRefDomOrDom(wrapEl),
           direction,
           prevDirection: state.direction,
         },
       );
 
       if (currentDirection && currentDirectionKey) {
-        // 方向与上次不同时同步
+        // 方向与上次不同时更新方向
         if (currentDirectionKey !== state.direction) {
           setState({
             direction: currentDirectionKey,
@@ -84,7 +104,7 @@ const Popper: React.FC<PopperProps> = ({ children, direction = 'top' }) => {
 
         // 前后visible状态均为false时跳过
         if (!self.lastVisible && !visible) {
-          self.refreshCount = 0; // 防止初次入场/重入场时抖动
+          self.refreshCount = 0; // 防止初次入场/重入场时气泡不必要的更新动画
           return;
         }
 
@@ -102,14 +122,20 @@ const Popper: React.FC<PopperProps> = ({ children, direction = 'top' }) => {
         self.refreshCount++;
       }
     },
-    f => _throttle(f, 0),
+    f => _throttle(f, 60),
   );
 
-  /** 初始化定位、默认触发变更方式(wrap滚动触发) */
+  /** 初始化定位、默认触发气泡更新方式(wrap滚动触发) */
   useEffect(() => {
     refresh();
-    wrapEl.current.addEventListener('scroll', refresh);
-  }, []);
+    const e = getRefDomOrDom(wrapEl) || window;
+
+    e.addEventListener('scroll', refresh);
+
+    return () => {
+      e.addEventListener('scroll', refresh);
+    };
+  }, [wrapEl]);
 
   /** show变更处理 */
   useUpdateEffect(() => {
@@ -123,30 +149,31 @@ const Popper: React.FC<PopperProps> = ({ children, direction = 'top' }) => {
   }, [state.show]);
 
   return (
-    <div className="wrap" ref={wrapEl} onClick={() => setState({ show: !state.show })}>
-      <div className="inner">
-        <span style={{ margin: 700, display: 'inline-block' }} ref={targetEl}>
-          {children}
-        </span>
-        {/* {children && React.cloneElement(children, { onClick: clickHandle })} */}
-        <Portal namespace="popper">
-          <animated.div
-            ref={popperEl}
-            style={{
-              transform: interpolate(
-                [spProps.xy, spProps.scale],
-                ([x, y], sc) => `translate3d(${x}px, ${y}px, 0) scale3d(${sc}, ${sc}, ${sc})`,
-              ),
-              opacity: spProps.opacity.interpolate(o => o),
-            }}
-            className={cls('fr-popper', state.direction && `__${state.direction}`)}
-          >
-            <span className={cls('fr-popper_arrow', state.direction && `__${state.direction}`)} />
-            <div className="fr-popper_content">提示一段提示</div>
-          </animated.div>
-        </Portal>
-      </div>
-    </div>
+    <>
+      <span
+        onClick={() => setState({show: !state.show})}
+        style={{margin: 700, display: 'inline-block'}}
+        ref={targetEl}
+      >
+        {children}
+      </span>
+      <Portal namespace="popper">
+        <animated.div
+          ref={popperEl}
+          style={{
+            transform: interpolate(
+              [spProps.xy, spProps.scale] as number[],
+              ([x, y]: any, sc) => `translate3d(${x}px, ${y}px, 0) scale3d(${sc}, ${sc}, ${sc})`,
+            ),
+            opacity: spProps.opacity.interpolate(o => o),
+          }}
+          className={cls('fr-popper', state.direction && `__${state.direction}`)}
+        >
+          <span className={cls('fr-popper_arrow', state.direction && `__${state.direction}`)}/>
+          <div className="fr-popper_content">提示一段提示</div>
+        </animated.div>
+      </Portal>
+    </>
   );
 };
 
--- src/docs/feedback/popper/demo.tsx
+++ src/docs/feedback/popper/demo.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useRef} from 'react';
 import Popper from '@lxjx/fr/lib/popper';
 import Button from '@lxjx/fr/lib/button';
 import sty from './demo.module.scss';
 
 const Demo = () => {
+  const wrap = useRef<HTMLDivElement>(null!);
+
   return (
     <div>
-      <Popper>
-        <Button>click</Button>
-      </Popper>
+      <div ref={wrap} className="wrap">
+        <div className="inner">
+          <Popper wrapEl={wrap}>
+            <Button>click</Button>
+          </Popper>
+        </div>
+      </div>
     </div>
   );
 };
