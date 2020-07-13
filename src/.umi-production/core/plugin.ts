760e3663d1a551c15bf75fc22b07e57f0847f397
lxj <1073830130@qq.com>
2020-07-13 23:25:54 +0800

    docs: update

--- src/.umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'C:/Users/lee/Desktop/fr/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
