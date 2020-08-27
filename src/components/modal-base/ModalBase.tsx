621750d46d60bccf28d7fc1e9edcab1d27a2591b
lxj <1073830130@qq.com>
2020-08-27 18:09:09 +0800

    update: ModalBase开发

--- src/components/modal-base/ModalBase.tsx
+++ src/components/modal-base/ModalBase.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { ModalBaseProps, TupleNumber } from 'm78/modal-base/types';
 import Portal from 'm78/portal';
 import { calcAlignment } from 'm78/modal-base/commons';
 import { useClickAway, useMeasure } from 'react-use';
 import { config, Transition } from '@lxjx/react-transition-spring';
-import { useFormState } from '@lxjx/hooks';
+import { useFormState, useLockBodyScroll, useSameState } from '@lxjx/hooks';
+
+import cls from 'classnames';
 
 // @lxjx\sass-base\var.scss line 128>
 const MODAL_Z_INDEX = 1800;
@@ -15,22 +17,49 @@ const ModalBase: React.FC<ModalBaseProps> = props => {
   const {
     namespace,
     alignment = DEFAULT_ALIGN,
-    mask,
+    mask = true,
     maskTheme,
     animationType = 'fade',
     mountOnEnter = true,
     unmountOnExit = false,
+    clickAwayClosable = true,
+    lockScroll = true,
+    className,
+    style,
   } = props;
 
+  /** 内容区域容器 */
+  const contRef = useRef<HTMLDivElement>(null!);
+
+  /** 代理defaultShow/show/onClose, 实现对应接口 */
   const [show, setShow] = useFormState<boolean>(props, false, {
     defaultValueKey: 'defaultShow',
     triggerKey: 'onClose',
     valueKey: 'show',
   });
 
+  const [cIndex, instances] = useSameState('fr_modal_metas', show, {
+    mask,
+    clickAwayClosable,
+  });
+
+  const nowZIndex = cIndex === -1 ? MODAL_Z_INDEX : cIndex + MODAL_Z_INDEX;
+
+  /** 监听大小 */
   const [bind, { width, height }] = useMeasure();
 
+  /** 内容区域的xy坐标 */
   const [pos, setPos] = useState([0, 0]);
+
+  // 滚动锁定
+  useLockBodyScroll(lockScroll && show);
+
+  // 无遮罩时，通过ClickAway来触发关闭，需要延迟一定的时间，因为用户设置的Modal开关可能会与ClickAway区域重叠
+  useClickAway(contRef, () => {
+    if ((clickAwayClosable && !mask && show) || shouldTriggerClose()) {
+      setTimeout(onClose, 150);
+    }
+  });
 
   useEffect(() => {
     calcPos();
@@ -44,21 +73,49 @@ const ModalBase: React.FC<ModalBaseProps> = props => {
   }
 
   function onClose() {
-    setShow(false);
+    shouldTriggerClose() && setShow(false);
   }
+
+  /** 根据该组件所有已渲染实例判断是否应开启mask */
+  function maskShouldShow() {
+    if (!mask) return false;
+
+    // 当前实例之前所有实例组成的数组
+    const beforeInstance = instances.slice(0, cIndex);
+
+    // 在该实例之前是否有任意一个实例包含mask
+    const beforeHasMask = beforeInstance.some(item => item.meta.mask);
+
+    return !beforeHasMask;
+  }
+
+  /** 根据前后实例判断是否需要触发Away点击关闭 */
+  function shouldTriggerClose() {
+    if (!clickAwayClosable) return true;
+
+    // 当前实例之前所有实例组成的数组
+    const afterInstance = instances.slice(cIndex + 1);
+
+    // 在该实例之前是否有任意一个实例包含mask
+    const afterHasAwayClosable = afterInstance.some(item => item.meta.clickAwayClosable);
+
+    return !afterHasAwayClosable;
+  }
+
+  console.log(style, shouldTriggerClose());
 
   return (
     <Portal namespace={namespace}>
-      {mask && (
+      {maskShouldShow() && mask && (
         <Transition
-          onClick={onClose}
+          // 有遮罩时点击遮罩来关闭
+          onClick={clickAwayClosable && onClose}
           toggle={show}
           type="fade"
-          // className={cls('m78-mask-node', dark ? 'm78-mask-b' : 'm78-mask')}
           mountOnEnter
           unmountOnExit
           className={maskTheme === 'dark' ? 'm78-mask-b' : 'm78-mask'}
-          style={{ zIndex: MODAL_Z_INDEX }}
+          style={{ zIndex: nowZIndex }}
         />
       )}
       <Transition
@@ -67,8 +124,9 @@ const ModalBase: React.FC<ModalBaseProps> = props => {
         config={config.stiff}
         mountOnEnter={mountOnEnter}
         unmountOnExit={unmountOnExit}
-        className="m78-modal-base"
-        style={{ left: pos[0], top: pos[1] }}
+        innerRef={contRef}
+        className={cls('m78-modal-base', className)}
+        style={{ ...style, left: pos[0], top: pos[1], zIndex: nowZIndex }}
       >
         <div className="m78-modal-base_calc-node" ref={bind} />
         <div style={{ width: 200, height: 160 }}>
@@ -82,12 +140,52 @@ const ModalBase: React.FC<ModalBaseProps> = props => {
 };
 
 function Temp() {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
+  const [show2, setShow2] = useState(true);
+  const [show3, setShow3] = useState(true);
+  const [show4, setShow4] = useState(true);
 
   return (
     <div>
-      <button onClick={() => setShow(!show)}>show</button>
-      <ModalBase show={show} onChange={() => setShow(false)} maskTheme="dark" />
+      <button
+        onClick={() => {
+          setShow(true);
+          setShow2(true);
+          setShow3(true);
+          setShow4(true);
+        }}
+      >
+        show
+      </button>
+      <ModalBase
+        animationType="slideTop"
+        style={{ border: '1px solid red' }}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+
+      <ModalBase
+        animationType="slideTop"
+        style={{ border: '1px solid orange' }}
+        show={show2}
+        onClose={() => setShow2(false)}
+      />
+
+      <ModalBase
+        animationType="slideTop"
+        style={{ border: '1px solid green' }}
+        show={show3}
+        onClose={() => setShow3(false)}
+      />
+
+      <ModalBase
+        animationType="slideTop"
+        style={{ border: '1px solid blue' }}
+        show={show4}
+        onClose={() => setShow4(false)}
+      />
+
+      <div style={{ height: 2000 }} />
     </div>
   );
 }
--- src/components/modal-base/types.ts
+++ src/components/modal-base/types.ts
@@ -1,8 +1,10 @@
 import { TransitionTypes } from '@lxjx/react-transition-spring';
+import { useClickAway } from 'react-use';
+import { ComponentBaseProps } from 'm78/types/types';
 
 export type TupleNumber = [number, number];
 
-export interface ModalBaseProps {
+export interface ModalBaseProps extends ComponentBaseProps {
   /** 默认显示状态，与show同时使用时无效 */
   defaultShow?: boolean;
   /** 手动控制modal的显示/隐藏, 与onClose搭配作为受控模式使用 */
@@ -13,6 +15,8 @@ export interface ModalBaseProps {
   namespace?: string;
   /** true | 是否显示遮罩 */
   mask?: boolean;
+  /** true | 点击内容区域以外是否关闭Modal */
+  clickAwayClosable?: boolean;
   /** 'light' | mask主题色 */
   maskTheme?: 'dark' | 'light';
   /** [0.5, 0.5] | 指定x/y轴位置, 值为 -1 ~ 1, 例：[0.5, 0.5] -> 居中， [1, 0] -> 右上， [1, 1] -> 右下 */
@@ -23,4 +27,10 @@ export interface ModalBaseProps {
   mountOnEnter?: boolean;
   /** false | Modal关闭后是否卸载其内容 */
   unmountOnExit?: boolean;
+  /** true | modal出现时锁定滚动条 */
+  lockScroll?: boolean;
+  /** TODO: 800 | 当传入onRemove时，会对其进行代理，当show为false在指定延迟内调用onRemove */
+  onRemoveDelay?: number;
+  /** TODO:  */
+  onRemove?: () => void;
 }
