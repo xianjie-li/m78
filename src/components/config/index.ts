25c994a5f0010268364818430ca5600a5ff52b45
lxj <1073830130@qq.com>
2021-04-18 19:58:11 +0800

    v1.0.0-beta.55

--- src/components/config/index.ts
+++ src/components/config/index.ts
@@ -1,21 +1,25 @@
-import React, { useContext } from 'react';
+import create from 'm78/seed';
+import React from 'react';
 
-export interface FrConfig {
-  /** empty组件的空状态图片 */
+interface M78SeedState {
+  /** 黑暗模式 */
+  darkMode?: boolean;
+  /** 全局配置Empty组件的空状态图片 */
   emptyNode?: React.ReactElement;
   /** Picture组件加载图片错误时的默认占位图 */
   pictureErrorImg?: string;
 }
 
-const context = React.createContext<FrConfig>({});
+const m78Seed = create<M78SeedState>({
+  state: {
+    darkMode: false,
+  },
+});
 
-function useConfig() {
-  return useContext(context);
-}
+m78Seed.subscribe(({ darkMode }) => {
+  if (typeof window !== 'undefined' && window.document) {
+    document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+  }
+});
 
-export default {
-  context,
-  Provider: context.Provider,
-  Consumer: context.Consumer,
-  useConfig,
-};
+export default m78Seed;
