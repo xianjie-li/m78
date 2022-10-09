a6145896205882c232cc1df7b0d2aaea64fe6581
Link <1073830130@qq.com>
2022-10-09 23:40:06 +0800

    monorepo整合

--- packages/build-tools/jest.config.js
+++ packages/build-tools/jest.config.js
@@ -6,4 +6,8 @@ export default {
   },
   extensionsToTreatAsEsm: [".ts", ".tsx"],
   testEnvironment: "jsdom",
+  // 严格的esm需要保留.js后缀, 这在jest中目前会导致模块未找到的报错, 通过此配置测试阶段去调.js后缀
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
